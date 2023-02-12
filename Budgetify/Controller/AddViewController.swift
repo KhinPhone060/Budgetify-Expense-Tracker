765924a0f6230662744a29e243bfbb56507e8cb6
KhinPhone060 <60284009+KhinPhone060@users.noreply.github.com>
2023-02-12 12:06:10 +0630

    design add transaction view controller

---
+++ Budgetify/Controller/AddViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddViewController.swift
+//  Budgetify
+//
+//  Created by Khin Phone Ei on 12/02/2023.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //self.hidesBottomBarWhenPushed = true
+    }
+    
+
+}
